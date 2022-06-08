1be19a51b5b76915786bee1557f08db2e04e7cc7
Eli Craig <elidcraig@gmail.com>
2022-06-07 20:30:32 -0700

    commit tree eb97bba3b9ab91717655dd86f27cb658b3eb59d4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+  
+  def initialize word
+    @word = word
+  end  
+
+  def match array
+    array.select { |word| word.chars.sort.join == @word.chars.sort.join}
+  end  
+end  
